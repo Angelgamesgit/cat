eb15ebe4530fa6f7a1b23f8f56897e10a9501aa1
tennmiyao <tennmiyao@gmail.com>
2026-04-06 08:04:23 +0900

    4

--- Assets/MyAssets/My_Scripts/bag script/Bag_PickedSystem.cs
+++ Assets/MyAssets/My_Scripts/bag script/Bag_PickedSystem.cs
@@ -1,29 +1,29 @@
 using UnityEngine;
 
-public class PickedSystem : MonoBehaviour
+public class Bag_PickedSystem : MonoBehaviour
 {
     [SerializeField] private GameObject itempickedUI; // UIのプレハブ(取得)
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    bool picked = false; // アイテムが拾われたかどうかのフラグ
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void StartBagMiniGame()
     {
         
+
+        gameObject.SetActive(false);
     }
     void OnTriggerEnter(Collider other)
     {
+
+        if (picked) return;
+        StartBagMiniGame();
         if (other.CompareTag("Item"))
         {
             //UIをOnにする
-itempickedUI.SetActive(true);
+        Bag_GameController.Instance.OpenBag(other.gameObject.GetComponent<Bag_ItemData>());
             //UIにアイテムの情報を渡す
             //itempickedUI.GetComponent<ItemPickedUI>().SetItem(pickedItem.bagItemData);
 
         }
     }
-    
 }
---
+++ Assets/MyAssets/My_Scripts/bag script/Bag_PickupItem.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Bag_PickupItem : MonoBehaviour
+{
+    //拾われる側は情報持っているくらいで十分
+
+    public Bag_ItemData itemData;
+    //必要ならタグも持たせる
+}
